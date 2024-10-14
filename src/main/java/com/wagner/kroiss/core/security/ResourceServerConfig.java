74b053f7f478942a612e8cf531e03bd284a93e5f
wagnerk78 <wagnerkroiss@gmail.com>
2024-10-14 16:25:14 -0300

    Autorizando o JWT

--- src/main/java/com/wagner/kroiss/core/security/ResourceServerConfig.java
+++ src/main/java/com/wagner/kroiss/core/security/ResourceServerConfig.java
@@ -2,13 +2,13 @@ package com.wagner.kroiss.core.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+
+import javax.crypto.spec.SecretKeySpec;
 
 @Configuration
 @EnableWebSecurity
@@ -18,10 +18,17 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                    .anyRequest().authenticated()
+                .anyRequest().authenticated()
                 .and()
-                .oauth2ResourceServer().opaqueToken();
+                .cors().and()
+                .oauth2ResourceServer().jwt();
     }
 
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        var secretKey = new SecretKeySpec("Uk6WfAY4QPrKdyiQ7i0Z8PDtaJoKSq9Ov999999999999999".getBytes(), "HmacSHA256");
+
+        return NimbusJwtDecoder.withSecretKey(secretKey).build();
+    }
 
 }
